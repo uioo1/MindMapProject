b6cf37cb79c02ece75aab645077a1ea44abac832
there <uioo1@naver.com>
2018-06-07 00:21:16 +0900

    일단 버튼누르면 textpanel에 있는 내용 콘솔에 출력됨

--- src/project/SplitPanel.java
+++ src/project/SplitPanel.java
@@ -1,32 +1,48 @@
 package project;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class SplitPanel {
 	JSplitPane split = new JSplitPane( JSplitPane.VERTICAL_SPLIT );
+	JPanel panel1;	//가운데 마인드맵패널 정의
+	JPanel panel2_1;	//모두 담는 제일 큰 패널 정의
+	JTextArea myDrawPanel;	//가운데 마인드맵패널 정의
+	JPanel panel3;	//오른쪽 속성 패널 정의
+	
 	public SplitPanel() {
         split.setDividerLocation( 200 );
         
         /////////////////가운데 마인드맵 패널
-        JPanel panel1 = new JPanel();
+        panel1 = new JPanel();
         panel1.setBackground(new Color(163, 202, 241));
         panel1.setLayout( new BorderLayout() );
         panel1.add( new JLabel( "CENTER panel" ), BorderLayout.CENTER );
+        //panel1.add( new JLabel( "여름" ), BorderLayout.SOUTH);        
 
-        /////////////////왼쪽 텍스트 패널
-        JPanel panel2_1 = new JPanel();
-        
-        JButton textbutton = new JButton("적용");
-		JTextArea myDrawPanel = new JTextArea(30,25);
+        /////////////////왼쪽 텍스트 패널+ 다 담는 패널
+        panel2_1 = new JPanel();
+        myDrawPanel = new JTextArea(30,25);
+        JButton textbutton = new JButton("적용");       
+		
 		panel2_1.setBackground(new Color(255, 255, 255));
 		panel2_1.setLayout(new BorderLayout(5,5));
 		panel2_1.add( new JScrollPane( myDrawPanel ), BorderLayout.CENTER );
 		panel2_1.add( textbutton, BorderLayout.SOUTH );
 		panel1.add(panel2_1, BorderLayout.WEST);
 		
+		ActionListener TextPanelActionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Tree().getTextPanel(myDrawPanel.getText());	//tree에 textPanel내용 넘겨주기
+			}
+		};
+	    textbutton.addActionListener(TextPanelActionListener);	//Action 리스너 달기
+	 
+				
         ////////////////오른쪽 설정 패널
-        JPanel panel3 = new JPanel();
+        panel3 = new JPanel();
         panel3.setBackground(new Color(174, 246, 160)); 
         panel3.setPreferredSize( new Dimension( 350, 350 ) );
     	GridLayout gridAttPane = new GridLayout(7,2,0,30);
@@ -55,6 +71,3 @@ public class SplitPanel {
 	}
 		
 }
-
-
-
---
+++ src/project/Tree.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Tree {
+	public Tree() {
+		
+	}
+	
+	public void getTextPanel(String text) {
+		System.out.println(text);		
+	}
+	
+}
