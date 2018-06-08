3d07954b9230fa39abdc85b7fdf84ce945aa74d8
there <uioo1@naver.com>
2018-06-08 22:24:51 +0900

    툴바에다가 툴팁을 달음

--- src/project/ToolBar.java
+++ src/project/ToolBar.java
@@ -15,23 +15,37 @@ public class ToolBar {
 		 JToolBar tool = new JToolBar("Kitae Menu");
 		 
 	     tool.setBackground(Color.gray);
-	     tool.add(new JButton("새로 만들기"));
+	     JButton newbtn = new JButton("새로 만들기");
+	     tool.add(newbtn);
+	     newbtn.setToolTipText("마인드 맵 파일을 새로 생성합니다");
 	     tool.addSeparator();
-	     tool.add(new JButton("열기"));
+	     JButton openbtn = new JButton("열기");
+	     tool.add(openbtn);
+	     openbtn.setToolTipText("마인드 맵 파일을 불러옵니다");
 	     tool.addSeparator();
-	     tool.add(new JButton("저장"));
-	     /* JButton btn3=new JButton(new ImageIcon("save.jpg"));//저장 이미지인데 내꺼에 이미지가 없는지 안나타남
+	     JButton savebtn = new JButton("저장");
+	     tool.add(savebtn);
+	     savebtn.setToolTipText("작업한 내용을 저장합니다");
+	     /*JButton btn3=new JButton(new ImageIcon("save.jpg"));//저장 이미지인데 내꺼에 이미지가 없는지 안나타남
 	     tool.add(btn3);*/
 	     tool.addSeparator();
-	     tool.add(new JButton("다른 이름으로 저장"));
+	     JButton save_otherbtn = new JButton("다른 이름으로 저장");
+	     tool.add(save_otherbtn);
+	     save_otherbtn.setToolTipText("작업한 내용을 새로운 파일로 저장합니다");
 	     tool.addSeparator();
-	     tool.add(new JButton("닫기"));
+	     JButton closebtn = new JButton("닫기");
+	     tool.add(closebtn);
+	     closebtn.setToolTipText("프로그램을 종료합니다");
 	     tool.addSeparator();
-	     tool.add(new JButton("적용"));
+	     JButton applybtn = new JButton("적용");
+	     tool.add(applybtn);
+	     applybtn.setToolTipText("텍스트 편집 내용을 마인드 맵에 적용합니다");
 	     tool.addSeparator();
-	     tool.add(new JButton("변경"));
+	     JButton changebtn = new JButton("변경");
+	     changebtn.setToolTipText("속성 변경 내용을 마인드 맵에 적용합니다");
+	     tool.add(changebtn);
 	     tool.addSeparator();
-	     // tool.add(new JTextField("text field"));
+
 	     JComboBox<String> combo = new JComboBox<String>();
 	     combo.addItem("click");
 	     combo.addItem("ㅗ");
